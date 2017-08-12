9f75851c8a3065569e0951e1c26bf8027eb2d88c
Matt Fellows <matt.fellows@onegeek.com.au>
2017-08-12 18:33:47 +1000

    chore(build): pact provider proxy no longer required

--- packaging/generate_readme_contents.rb
+++ packaging/generate_readme_contents.rb
@@ -2,7 +2,6 @@ require 'erb'
 require 'pact/version'
 require 'pact/mock_service/version'
 require 'pact/support/version'
-require 'pact/provider/proxy/version'
 require 'pact/provider_verifier/version'
 
 puts ERB.new(ARGF.read).result(binding)
