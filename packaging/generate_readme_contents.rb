97a60f9c53e63edfda4417893303017f20666afe
Beth Skurrie <beth@bethesque.com>
2017-10-01 14:55:57 +1100

    feat(readme): add detailed usage notes generated from each tool

--- packaging/generate_readme_contents.rb
+++ packaging/generate_readme_contents.rb
@@ -5,4 +5,7 @@ require 'pact/support/version'
 require 'pact/provider_verifier/version'
 require 'pact_broker/client/version'
 
+pact_mock_service_usage = `bundle exec pact-mock-service help` + `bundle exec pact-mock-service help service`
+pact_provider_verifier_usage = `bundle exec pact-provider-verifier help`
+pact_publish_usage = `bundle exec pact-publish help`
 puts ERB.new(ARGF.read).result(binding)
