08e54209edb962f45d1b22d95ab989ec89328e8d
Beth Skurrie <beth@bethesque.com>
2018-02-08 11:48:28 +1100

    feat(pact-broker cli): allow custom certificates to be used by setting SSL_CERT_FILE and SSL_CERT_DIR
    Closes: https://github.com/pact-foundation/pact-ruby-standalone/issues/11

--- packaging/pact-broker.rb
+++ packaging/pact-broker.rb
@@ -12,4 +12,17 @@ class Thor
   end
 end
 
+# Travelling Ruby sets its own CA cert bundle in lib/ruby/bin/ruby_environment
+# and creates backup environment variables for the original SSL_CERT values.
+# Restore the original values here *if they are present* so that we can connect to
+# a broker with a custom SSL certificate.
+
+if ENV['ORIG_SSL_CERT_DIR'] && ENV['ORIG_SSL_CERT_DIR'] != ''
+  ENV['SSL_CERT_DIR'] = ENV['ORIG_SSL_CERT_DIR']
+end
+
+if ENV['ORIG_SSL_CERT_FILE'] && ENV['ORIG_SSL_CERT_FILE'] != ''
+  ENV['SSL_CERT_FILE'] = ENV['ORIG_SSL_CERT_FILE']
+end
+
 PactBroker::Client::CLI::Broker.start
