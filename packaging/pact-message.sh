1feec039cd5486495736dfa86810aa1733f4d5ea
Matt Fellows <matt.fellows@onegeek.com.au>
2018-03-24 16:39:19 +1100

    feat(pact-message): add pact-message binary to release

---
+++ packaging/pact-message.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+set -e
+
+SOURCE="$0"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  TARGET="$(readlink "$SOURCE")"
+  if [[ $TARGET == /* ]]; then
+    SOURCE="$TARGET"
+  else
+    DIR="$( dirname "$SOURCE" )"
+    SOURCE="$DIR/$TARGET" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  fi
+done
+RDIR="$( dirname "$SOURCE" )"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+# Figure out where this script is located.
+LIBDIR="`cd \"$DIR\" && cd ../lib && pwd`"
+
+# Tell Bundler where the Gemfile and gems are.
+export BUNDLE_GEMFILE="$LIBDIR/vendor/Gemfile"
+unset BUNDLE_IGNORE_CONFIG
+unset RUBYGEMS_GEMDEPS
+export BUNDLE_FROZEN=1
+
+# Run the actual app using the bundled Ruby interpreter, with Bundler activated.
+exec "$LIBDIR/ruby/bin/ruby" -rreadline -rbundler/setup -I "$LIBDIR/app/lib" "$LIBDIR/app/pact-message.rb" "$@"
