f333303310675246b142f3d1c0c264c794cbafd6
Beth Skurrie <beth@bethesque.com>
2018-07-10 20:31:09 +1000

    chore: update destination of git push
    [ci-skip]

--- script/release.sh
+++ script/release.sh
@@ -6,5 +6,5 @@ bundle exec rake package:update
 bundle exec bump ${1:-minor} --no-commit
 bundle exec rake generate_changelog
 git add VERSION CHANGELOG.md
-git commit -m "chore(release): version $(cat VERSION)" && git push
+git commit -m "chore(release): version $(cat VERSION)" && git push origin master
 bundle exec rake tag_for_release
