c858b63ad834f75cbcb91d8bf4590819994d2dbf
Beth Skurrie <beth@bethesque.com>
2018-07-10 20:53:30 +1000

    chore: update release-from-travis.sh
    [ci skip]

--- script/release-from-travis.sh
+++ script/release-from-travis.sh
@@ -8,6 +8,7 @@ fi
 git remote -v
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
+git remote remove origin
 git remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
 
 ./script/release.sh
