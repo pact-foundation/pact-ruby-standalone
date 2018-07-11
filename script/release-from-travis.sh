efb354c4e6c4beb9b99ceb78437864362edbb55a
Beth Skurrie <beth@bethesque.com>
2018-07-11 10:41:08 +1000

    chore: ensure new commits go on a branch
    [ci skip]

--- script/release-from-travis.sh
+++ script/release-from-travis.sh
@@ -5,10 +5,10 @@ if [ -z "$TRAVIS" ]; then
   exit 1
 fi
 
-git remote -v
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git remote remove origin
 git remote add origin https://${GITHUB_ACCESS_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git > /dev/null 2>&1
+git checkout ${TRAVIS_BRANCH}
 
 ./script/release.sh
