83b372a89c8ff6f57f9abb5c51ccdd6992c51165
Beth Skurrie <beth@bethesque.com>
2020-02-14 08:37:54 +1100

    chore: update update.sh

--- script/update.sh
+++ script/update.sh
@@ -2,6 +2,19 @@
 
 source script/docker-functions
 
+git checkout packaging/Gemfile.lock
 rm -rf tmp
-docker_build_bundle_base >/dev/null 2>&1
-bundle_update_on_docker
+docker_build_bundle_base
+output=$(bundle_update_on_docker)
+echo "${output}"
+updated_pact_gems=$(echo "${output}" | grep "pact" | grep "(was " | cut -d " " -f 2 -f 3 | uniq | ruby -e 'puts ARGF.read.split("\n").join(", ")')
+
+git add packaging/Gemfile.lock
+
+if [ -n "${updated_pact_gems}" ]; then
+  commit_message="feat(gems): update to ${updated_pact_gems}"
+else
+  commit_message="feat(gems): update non-pact gems"
+fi
+
+git commit -m "${commit_message}"
