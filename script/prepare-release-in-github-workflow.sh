d75e446371d81d59cdaa1638c92fb5fa3f289e76
Beth Skurrie <beth@bethesque.com>
2020-07-17 20:41:24 +1000

    chore: update release workflow

--- script/prepare-release-in-github-workflow.sh
+++ script/prepare-release-in-github-workflow.sh
@@ -4,15 +4,20 @@ set -e
 
 bundle exec bump minor --no-commit
 bundle exec rake generate_changelog
-VERSION=$(cat VERSION)
-TAG="v${VERSION}"
+version=$(cat VERSION)
+tag="v${VERSION}"
+echo "::set-env name=VERSION::${version}"
+echo "::set-env name=TAG::${tag}"
 
 bundle exec rake package
 pushd pkg; for file in *.{zip,gz}; do sha1sum -b "$file" > "${file}.checksum"; done; popd;
 cat pkg/*.checksum > pkg/pact-`cat VERSION`.checksum
 
+
+echo "::set-env name=FILES::$(find pkg -maxdepth 1 -mindepth 1)"
+
 git add VERSION CHANGELOG.md
-git commit -m "chore(release): version ${VERSION}
+git commit -m "chore(release): version ${version}
 [ci-skip]"
-#git push origin master
-#git tag -a ${TAG} -m "chore(release): version ${VERSION}" && git push origin ${TAG}
+git push origin master
+git tag -a ${tag} -m "chore(release): version ${version}" && git push origin ${TAG}
