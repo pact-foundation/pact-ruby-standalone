d2c3b0793594eced1609dca6e49b54d29fcdd1d0
Beth Skurrie <beth@bethesque.com>
2020-07-17 21:11:20 +1000

    chore: generate release notes after build

--- script/prepare-release-in-github-workflow.sh
+++ script/prepare-release-in-github-workflow.sh
@@ -7,18 +7,18 @@ bundle exec rake generate_changelog
 version=$(cat VERSION)
 tag="v${version}"
 
-bundle exec rake generate_release_notes[$tag]
 
 echo "::set-env name=VERSION::${version}"
 echo "::set-env name=TAG::${tag}"
-echo "::set-env name=RELEASE_BODY::$(cat build/RELEASE_NOTES.md)"
 
 bundle exec rake package
 pushd pkg; for file in *.{zip,gz}; do sha1sum -b "$file" > "${file}.checksum"; done; popd;
 cat pkg/*.checksum > pkg/pact-`cat VERSION`.checksum
 
-
 echo "::set-env name=FILES::$(find pkg -maxdepth 1 -mindepth 1 | paste -sd " " -)"
+
+bundle exec rake generate_release_notes[$tag]
+echo "::set-env name=RELEASE_BODY::$(cat build/RELEASE_NOTES.md)"
 
 git add VERSION CHANGELOG.md
 git commit -m "chore(release): version ${version}
