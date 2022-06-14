218c2dc7eadb3bd98f72325e99f9d0d6eb85bce3
Beth Skurrie <beth@bethesque.com>
2022-06-14 10:47:48 +1000

    chore: fix generation of publish-provider-contract docs

--- packaging/generate_readme_contents.rb
+++ packaging/generate_readme_contents.rb
@@ -13,5 +13,5 @@ pact_publish_usage = `bundle exec pact-broker help publish`
 pact_broker_can_i_deploy_usage = `bundle exec pact-broker help can-i-deploy`
 pact_docs_usage = `bundle exec pact help docs`
 pact_message_usage = `bundle exec pact-message help`
-pactflow_publish_provider_contract_usage = `bundle exec pactflow publish-provider-contract help`
+pactflow_publish_provider_contract_usage = `bundle exec pactflow help publish-provider-contract`
 puts ERB.new(ARGF.read).result(binding)
